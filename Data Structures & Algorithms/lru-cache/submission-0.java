ab9bc5ca5f6e750158df0cfdbc87d4327d54c1e3
再见一面 <162898689+zaijianyimian@users.noreply.github.com>
2026-06-20 10:36:23 +0800

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.java
@@ -0,0 +1,22 @@
+
+class LRUCache extends LinkedHashMap<Integer, Integer> {
+    private final int capacity;
+
+    public LRUCache(int capacity) {
+        super(capacity, 0.75f, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+}
