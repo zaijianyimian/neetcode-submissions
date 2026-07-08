57cd305d6131bc99b6fa0ca19ddaeb0faa0668ae
再见一面 <162898689+zaijianyimian@users.noreply.github.com>
2026-07-08 09:56:12 +0800

    Add: counting-bits - submission-2

---
+++ Data Structures & Algorithms/counting-bits/submission-2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] dp = new int[n + 1];
+        for(int i = 0; i <= n;i ++){
+            if(i % 2 == 0){
+                dp[i] = dp[i / 2];
+            }else{
+                dp[i] = dp[i / 2] + 1;
+            }
+        }
+        return dp;
+    }
+}
